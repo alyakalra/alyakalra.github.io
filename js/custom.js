7e0551638eab6140b60d0b5ff848601b474e114d
alyakalra <alskals2000@gmail.com>
2016-07-21 10:38:56 -0700

    Add JS elements

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
-
-
-if $("readmore").click(function(){
-		$("p").show();
-
-		});
-else {
 	$("p").hide();
-	}
+	$(".giff").hide();
+
+	$("#readless").click(function(){
+		$("p").hide();
+	});
+
+
+	$("#readmore").click(function(){
+	 	$("p").show();
+
+	 });
+
+	
+	$(".gif").click(function(){
+		$(this).attr("src","images/200.gif")
+		$(".gif").click(function(){
+		$(this).attr("src","images/giphy.gif")
+	});
+
+	});
+
+	
+
+
+
 });	
 
 	
